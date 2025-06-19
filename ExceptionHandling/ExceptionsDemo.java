e1b17e9053ec5af0afa6982a3661712c6c5c91a7
Sana Gul <sanashaaah4@gmail.com>
2025-06-19 17:47:13 +0500

    feat: add catch blocks for FileNotFoundException, IOException, and ParseException

--- ExceptionHandling/ExceptionsDemo.java
+++ ExceptionHandling/ExceptionsDemo.java
@@ -2,19 +2,27 @@ package ExceptionHandling;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        // sayHello(null);
         try {
             var reader = new FileReader("file.txt");
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
+
         } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not read data");
+        } catch (ParseException e) {
+            System.out.println("Could not parse data");
         }
-        System.out.println("File opened");
     }
 
-    public static void sayHello(String name) {
+    public static void sayHello(final String name) {
         // Null Pointer Exception
         // Calling a method to null
         // System.out.println(name.toUpperCase());
