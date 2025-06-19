afe47027177ac8f06e820948a9cd2ac0d015c81b
Sana Gul <sanashaaah4@gmail.com>
2025-06-19 17:09:59 +0500

    feat: handle FileNotFoundException using try-catch block

--- ExceptionHandling/ExceptionsDemo.java
+++ ExceptionHandling/ExceptionsDemo.java
@@ -1,8 +1,17 @@
 package ExceptionHandling;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
+        // sayHello(null);
+        try {
+            var reader = new FileReader("file.txt");
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("File opened");
     }
 
     public static void sayHello(String name) {
