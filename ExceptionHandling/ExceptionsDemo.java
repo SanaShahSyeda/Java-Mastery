71f17246084a9f454ebd7a9d1923e6f7059d35c1
Sana Gul <sanashaaah4@gmail.com>
2025-06-19 18:51:06 +0500

    feat: add finally block to close file resource

--- ExceptionHandling/ExceptionsDemo.java
+++ ExceptionHandling/ExceptionsDemo.java
@@ -1,24 +1,24 @@
 package ExceptionHandling;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
+        FileReader reader = null;
         try {
-            var reader = new FileReader("file.txt");
-            var value = reader.read();
-            new SimpleDateFormat().parse("");
-
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
+            reader = new FileReader("file.txt");
+            reader.read();
         } catch (IOException e) {
             System.out.println("Could not read data");
-        } catch (ParseException e) {
-            System.out.println("Could not parse data");
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
