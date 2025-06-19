99c408d14dd02c87effac074d72ddca4f49f68ed
Sana Gul <sanashaaah4@gmail.com>
2025-06-19 15:02:10 +0500

    feat: add NulPointer Exception demo

---
+++ ExceptionHandling/ExceptionsDemo.java
@@ -0,0 +1,13 @@
+package ExceptionHandling;
+
+public class ExceptionsDemo {
+    public static void show() {
+        sayHello(null);
+    }
+
+    public static void sayHello(String name) {
+        // Null Pointer Exception
+        // Calling a method to null
+        // System.out.println(name.toUpperCase());
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+import ExceptionHandling.ExceptionsDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        ExceptionsDemo.show();
+    }
+}
