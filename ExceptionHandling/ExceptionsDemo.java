0fcd415b3de6d9c1a160364fdb6a763915691c24
Sana Gul <sanashaaah4@gmail.com>
2025-06-26 10:47:24 +0500

    feat: use try-with-resources
    - Removed finally block and declared resource within try()
    - Supports multiple resource declarations
    - Compiles into try-finally block
    - Automatically closes the resource after execution

--- ExceptionHandling/ExceptionsDemo.java
+++ ExceptionHandling/ExceptionsDemo.java
@@ -1,30 +1,17 @@
 package ExceptionHandling;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...")) {
             reader.read();
         } catch (IOException e) {
             System.out.println("Could not read data");
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-    }
-
-    public static void sayHello(final String name) {
-        // Null Pointer Exception
-        // Calling a method to null
-        // System.out.println(name.toUpperCase());
     }
 }
