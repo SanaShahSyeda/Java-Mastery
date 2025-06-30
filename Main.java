b71480253ca702340f92ea7a9341c06a5e68c6c1
Sana Gul <sanashaaah4@gmail.com>
2025-06-30 20:06:06 +0500

    feat(Generics): add an example demonstrating Multiple Type parameters
    - Updated Utils.java with method to accept multiple generic types
    - Modified Main.java to demonstrate Utils method

--- Main.java
+++ Main.java
@@ -4,9 +4,6 @@ import Generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        int max = Utils.max(1, 2);
-        System.out.println(max);
-
-        System.out.println(Utils.max(new User(20), new User(30)));
+        Utils.print(1, 10);
     }
 }
