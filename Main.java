b411d9cc635deb8a6847838984a10c1734b8e465
Sana Gul <sanashaaah4@gmail.com>
2025-06-30 15:39:33 +0500

    feat(Generics): add example demonstrating Wrapper class

--- Main.java
+++ Main.java
@@ -2,17 +2,8 @@ import Generics.GenericList;
 
 public class Main {
     public static void main(String[] args) {
-        var intItems = new GenericList<Integer>();
-        intItems.add(1);
-        intItems.add(2);
-        intItems.add(3);
-        Integer x = intItems.get(1);
-        System.out.println(x);
-
-        var moviesItems = new GenericList<String>();
-        moviesItems.add("Shawshank Redemption");
-        moviesItems.add("The pursuit of Happiness");
-        moviesItems.add("A Beautiful mind");
-        System.out.println("Value of item at index 2: " + moviesItems.get(2));
+        GenericList<Integer> numbers = new GenericList<Integer>();
+        numbers.add(1); // Boxing
+        int number = numbers.get(0); // Unboxing
     }
 }
