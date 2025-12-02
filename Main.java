d03aeef9981c96f2dc1e43b9f03141d3c9bc2cac
Sana Gul <sanashaaah4@gmail.com>
2025-12-02 10:39:17 +0500

    refactor(main): remove demo code from Main
    Remove leftover demo code and commented examples from `Main.java` to keep entrypoint minimal.

--- Main.java
+++ Main.java
@@ -5,11 +5,5 @@ import Generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new Instructor(10);
-        Utils.printUser(new Instructor(20));
-
-        var users = new GenericList<User>();
-        Utils.addUsers(users);
-        Utils.printUsers(users);
     }
 }
