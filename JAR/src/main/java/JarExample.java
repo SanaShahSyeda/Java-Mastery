d150ee35812d90dad6612e43ba08cd818c4d5996
Sana Gul <sanashaaah4@gmail.com>
2026-01-12 00:20:20 +0500

    feat(JAR): add JarExample file importing an external library

---
+++ JAR/src/main/java/JarExample.java
@@ -0,0 +1,9 @@
+package JAR;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class JarExample {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("thin vs fat jar"));
+    }
+}
